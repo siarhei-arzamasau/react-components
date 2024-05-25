cefaa37865aa70478508839b8a9027dfdd86bed6
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-05-25 23:45:09 +0300

    add onCLick

--- src/star-rating.component.jsx
+++ src/star-rating.component.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './star-rating.styles.css';
 
 const StarRating = () => {
   const maxRating = 5;
 
+  const [currentRating, setCurrentRating] = useState(0);
+
   return (
-    <div>
+    <div className='star-rating-container'>
+      currentRating: { currentRating }
       {
-        [...Array(5)].map((_, idx) => <p key={idx}>{idx + 1}</p>)
+        [...Array(5)].map((_, idx) => {
+          const ratingValue = idx + 1;
+          
+          return <p onClick={() => setCurrentRating(ratingValue)} key={idx}>{ratingValue}</p>
+        })
       }
     </div>
   )
